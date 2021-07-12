810f74380043c914c479642ccfbe8c64ae4f021e
Tomasz Grajewski <contact@tomaszgrajewski.com>
2021-07-12 14:26:23 +0200

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// Doesn't show the error properly in the browser dev tools
+console.logz('Hello World!');  // Intentional typo
+
+
+// Works fine (error is properly shown)
+// setTimeout(function() {
+//     console.logz('Hello World!');  // Intentional typo
+// }, 1000);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: [
+      {
+        directory: 'dist'
+      }
+    ]
+  }
+};
